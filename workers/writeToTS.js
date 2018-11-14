b16c96865a6f1b72423e23d0b00b1ed1376968bb
Nick Calibey <nick.calibey@gmail.com>
2018-11-14 15:58:06 -0600

    refactored timescale worker and separated queries into separate file

--- workers/writeToTS.js
+++ workers/writeToTS.js
@@ -7,68 +7,44 @@ const client = new Client({
 });
 client.connect();
 
+const INSERT = require('./timescaledb_queries');
+
 const writeToDB = (msg) => {
   const json = JSON.parse(msg.content);
   let { eType, timestamp, metadata } = json;
-  timestamp = timestamp / 1000;
+  timestamp /= 1000;
   let text;
   let values;
 
   if (eType === 'link_clicks') {
     let { linkText, targetURL } = json;
-
-    text = 'INSERT INTO link_clicks(link_text, target_url, client_time, metadata) VALUES ($1, $2, to_timestamp($3), $4) RETURNING *';
+    text = INSERT.link_click;
     values = [linkText, targetURL, timestamp, metadata];
-
-    client.query(text, values, (err, res) => {
-      console.log(err ? err.stack : res.rows[0]);
-    });
   } else if (eType === 'clicks') {
     let { target_node, buttons, x, y } = json;
-
-    text = 'INSERT INTO clicks(target_node, buttons, x, y, client_time, metadata) VALUES ($1, $2, $3, $4, to_timestamp($5), $6) RETURNING *';
+    text = INSERT.click;
     values = [target_node, buttons, x, y, timestamp, metadata];
-
-    client.query(text, values, (err, res) => {
-      console.log(err ? err.stack : res.rows[0]);
-    });
   } else if (eType === 'mouse_moves') {
     let { x, y } = json;
-
-    text = 'INSERT INTO mouse_moves(x, y, client_time, metadata) VALUES ($1, $2, to_timestamp($3), $4) RETURNING *';
+    text = INSERT.mouse_move;
     values = [x, y, timestamp, metadata];
-
-    client.query(text, values, (err, res) => {
-      console.log(err ? err.stack : res.rows[0]);
-    });
   } else if (eType === 'key_presses') {
     let { key } = json;
-
-    text = 'INSERT INTO key_presses(key, client_time, metadata) VALUES ($1, to_timestamp($2), $3) RETURNING *';
+    text = INSERT.key_press;
     values = [key, timestamp, metadata];
-
-    client.query(text, values, (err, res) => {
-      console.log(err ? err.stack : res.rows[0]);
-    });
   } else if (eType === 'pageviews') {
     let { url, title } = json;
-
-    text = 'INSERT INTO pageviews(url, title, client_time, metadata) VALUES ($1, $2, to_timestamp($3), $4) RETURNING *';
+    text = INSERT.pageview;
     values = [url, title, timestamp, metadata];
-
-    client.query(text, values, (err, res) => {
-      console.log(err ? err.stack : res.rows[0]);
-    });
   } else if (eType === 'form_submissions') {
     let { data } = json;
-
-    text = 'INSERT INTO form_submissions(data, client_time, metadata) VALUES ($1, to_timestamp($2), $3) RETURNING *';
+    text = INSERT.form_submission;
     values = [data, timestamp, metadata];
-
-    client.query(text, values, (err, res) => {
-      console.log(err ? err.stack : res.rows[0]);
-    });
   }
+
+  client.query(text, values, (err, res) => {
+    console.log(err ? err.stack : res.rows[0]);
+  });
 }
 
 module.exports = writeToDB;
