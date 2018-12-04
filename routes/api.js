c4ed24fdbe479e8305ec254098b4efed2a6cb020
Nick Calibey <nick.calibey@gmail.com>
2018-12-04 15:12:09 -0600

    re-implemented Producer

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
-var client = require('../connection');
-var moment = require('moment');
-var amqp = require('amqplib');
-let size = 0;
+const express = require('express');
+const router = express.Router();
+const client = require('../connection');
+
+/* Kafka Producer Configuration */
+const { Producer } = require('sinek');
+const kafkaConfig = require('../kafkaConfig');
+const partitions = 6;
+const compressionType = 0; // no compression
+const topic = 'events';
+const producer = new Producer(kafkaConfig, topic, partitions);
+
+producer.connect();
+producer.on('error', error => console.log(error));
 
 /* GET users listing. */
 router.get('/events', function(req, res, next) {
@@ -13,78 +21,12 @@ router.get('/events', function(req, res, next) {
   res.send([]);
 });
 
+/* Tracker API Endpoint */
 router.post('/events', (req, res) => {
-  const json = JSON.parse(req.body);
-  console.log(req.body);
-  // let { eType, linkText, targetURL, timestamp } = json.eventAttrs;
-  // timestamp = moment.utc(timestamp).format();
-  //
-  // const text = 'INSERT INTO link_clicks(link_text, target_url, local_time) VALUES ($1, $2, $3) RETURNING *';
-  // const values = [linkText, targetURL, timestamp];
-  //
-  // const text2 = 'INSERT INTO metadata(url, user_agent, page_title, cookie_allowed, language, event_type, event_id, time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
-  // let fres;
-  //
-  // client.query(text, values, (err, res) => {
-  //   console.log(err ? err.stack : res.rows[0]);
-  //   if (res) {
-  //     fres = res.rows[0];
-  //     const {url, userAgent, pageTitle, cookieAllowed, language} = json.metadata;
-  //     console.log(json.metadata.uuid);
-  //
-  //     const values2 = [url, userAgent, pageTitle, cookieAllowed, language, eType, fres.id, timestamp];
-  //
-  //     client.query(text2, values2, (err, res) => {
-  //       console.log(err ? err.stack : res.rows[0]);
-  //     })
-  //   }
-  // });
-
-  res.send('');
-})
-
-router.post('/events/mousemoves', (req, res, next) => {
-  const json = JSON.parse(req.body);
-  const events = json.eventAttrs;
-  const { metadata } = json;
-
-  events.forEach(event => {
-    let { x, y, timestamp } = event;
-    timestamp = moment.utc(timestamp).format();
-    const text = 'INSERT INTO mouse_moves(x, y, local_time, metadata) VALUES ($1, $2, $3, $4) RETURNING *';
-    const values = [x, y, timestamp, metadata]
-
-    client.query(text, values, (err, res) => {
-      console.log(err ? err.stack : res.rows[0]);
-    });
-  })
-
-  res.send('');
-})
-
-router.post('/testing', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  let json = req.body;
+  const json = req.body;
 
   try {
-    json.events.forEach(event => {
-      const eType = event['eType'];
-      event['metadata'] = json.metadata;
-
-      amqp.connect('amqp://localhost').then(conn => {
-        return conn.createChannel().then(ch => {
-          const ex = 'clientEvents';
-          const ok = ch.assertExchange(ex, 'direct', {durable: false});
-
-          return ok.then(() => {
-            ch.publish(ex, eType, Buffer.from(JSON.stringify(event)));
-            console.log(" [x] Sent %s: '%s'", eType, JSON.stringify(event));
-            return ch.close();
-          });
-        }).finally(() => conn.close());
-      }).catch(console.warn);
-    });
+    producer.buffer(topic, undefined, { json }, compressionType);
 
     res.send(JSON.stringify({"success": true}));
   } catch (e) {
@@ -93,6 +35,54 @@ router.post('/testing', (req, res, next) => {
       "error": String(e)
     }));
   }
-})
+});
+
+// router.post('/events/mousemoves', (req, res, next) => {
+//   const json = JSON.parse(req.body);
+//   const events = json.eventAttrs;
+//   const { metadata } = json;
+//
+//   events.forEach(event => {
+//     let { x, y, timestamp } = event;
+//     timestamp = moment.utc(timestamp).format();
+//     const text = 'INSERT INTO mouse_moves(x, y, local_time, metadata) VALUES ($1, $2, $3, $4) RETURNING *';
+//     const values = [x, y, timestamp, metadata]
+//
+//     client.query(text, values, (err, res) => {
+//       console.log(err ? err.stack : res.rows[0]);
+//     });
+//   })
+//
+//   res.send('');
+// })
+//
+// router.post('/testing', (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   let json = req.body;
+//
+//   try {
+//     json.events.forEach(event => {
+//       const eType = event['eType'];
+//       event['metadata'] = json.metadata;
+//
+//       amqp.connect('amqp://localhost').then(conn => {
+//         return conn.createChannel().then(ch => {
+//           const ex = 'clientEvents';
+//           const ok = ch.assertExchange(ex, 'direct', {durable: false});
+//
+//           return ok.then(() => {
+//             ch.publish(ex, eType, Buffer.from(JSON.stringify(event)));
+//             console.log(" [x] Sent %s: '%s'", eType, JSON.stringify(event));
+//             return ch.close();
+//           });
+//         }).finally(() => conn.close());
+//       }).catch(console.warn);
+//     });
+//
+//
+//   } catch (e) {
+//
+// })
 
 module.exports = router;
