5ca1a30789cc7ee475ab62146892baaba2d91088
Nick Calibey <nick.calibey@gmail.com>
2018-12-04 15:11:46 -0600

    refactored worker

--- workers/worker.js
+++ workers/worker.js
@@ -1,6 +1,3 @@
-// const amqp = require('amqplib');
-// const all = require('bluebird').all;
-// const basename = require('path').basename;
 const writeToDB = require('./writeToDB');
 const { Consumer } = require('sinek');
 const kafkaConfig = require('../kafkaConfig');
@@ -20,20 +17,3 @@ consumer.connect(withBackpressure).then(_ => {
 });
 
 consumer.on('error', error => console.log(error));
-
-// amqp.connect('amqp://localhost').then(conn => {
-//   process.once('SIGINT', () => conn.close());
-//   return conn.createChannel().then(ch => {
-//     const ex = 'clientEvents';
-//     let ok = ch.assertExchange(ex, 'direct', {durable: false});
-//     ok = ok.then(() => ch.assertQueue('', {exclusive: true}));
-//     ok = ok.then(qok => {
-//       const queue = qok.queue;
-//
-//       return all(events.map(event => ch.bindQueue(queue, ex, event))).then(() => queue);
-//     });
-//     ok = ok.then(queue => ch.consume(queue, writeToDB, {noAck: true}));
-//
-//     return ok.then(() => console.log(' [*] Waiting for logs. To exit press CTRL+C'));
-//   });
-// }).catch(console.warn);
