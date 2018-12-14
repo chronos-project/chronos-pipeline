0825e3665b9200372ced110f67dc4e1d56b24a02
Nick Calibey <nick.calibey@gmail.com>
2018-12-14 13:37:56 -0600

    Send 401 response instead of raising exception
    Co-Authored-By: sasha prodan <prodan.sasha@gmail.com>

--- server/lib/requires_access_key_middleware.js
+++ server/lib/requires_access_key_middleware.js
@@ -4,8 +4,6 @@ module.exports = function requiresAllowedOrigin(req, res, next) {
   if (config.ACCESS_KEY === req.body['ACCESS_KEY']) {
     return next();
   } else {
-    var err = new Error('Unauthorized Access');
-    err.status = 401;
-    return next(err);
+    res.status(401).send('Unauthorized Access');
   }
 };
