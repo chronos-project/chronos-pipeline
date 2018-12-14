8ced844062c46b6b9dfcff8d2d6471bfcc228e0b
Nick Calibey <nick.calibey@gmail.com>
2018-12-14 13:37:22 -0600

    Fix typo, remove CORS configuration, and refactor
    Co-Authored-By: sasha prodan <prodan.sasha@gmail.com>

--- server/app.js
+++ server/app.js
@@ -7,41 +7,23 @@ const config = require('./config');
 const bodyParser = require('body-parser');
 
 const requiresAllowedOrigin = require('./lib/allowed_origins_middleware');
-const requiresAllowedOrigin = require('./lib/require_access_key_middleware');
+const requiresAccessKey = require('./lib/requires_access_key_middleware');
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
 const app = express();
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (config.ALLOWED_ORIGINS.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}
-
-// app.use(bodyParser.text());
+app.use(cors())
 app.use(logger('dev'));
-app.use(express.json({ type: (req) => {
-  // if () {
-  //
-  // }true
-    return true;
-  }
-}));
+app.use(express.json({ type: (req) => true }));
 
 app.use(express.urlencoded({ extended: false }));
-// app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(cookieParser());
 
 app.use(requiresAllowedOrigin);
 app.use(requiresAccessKey);
 
-app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
