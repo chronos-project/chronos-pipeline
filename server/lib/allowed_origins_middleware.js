ca44200dc789d445ce31ccc393da436d6892f7f5
Sasha Prodan <prodan.sasha@gmail.com>
2018-12-20 14:41:17 -0600

    allow rest tools to send request

--- server/lib/allowed_origins_middleware.js
+++ server/lib/allowed_origins_middleware.js
@@ -1,7 +1,7 @@
 const config = require('../config');
 
 module.exports = function requiresAllowedOrigin(req, res, next) {
-  if (config.ALLOWED_ORIGINS.includes(req.get('Origin'))) {
+  if (config.ALLOWED_ORIGINS.includes(req.get('Origin')) || !req.get('Origin')) {
     return next();
   } else {
     res.status(401).send('Unauthorized Access');
